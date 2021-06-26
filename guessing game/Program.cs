e53fccce5b1470fa56f4710c4d338afd5b2db7a3
jacam666 <ja6cam@gmail.com>
2021-06-26 21:02:46 +0100

    Add project files.

---
+++ guessing game/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "cameron";
+            string guess = "";
+            while (guess != secretWord)
+            {
+                Console.Write("Enter your guess : ");
+                guess = Console.ReadLine();
+            }
+            Console.Write("You win!!!!");
+        }
+    }
+}
